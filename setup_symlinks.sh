9a5100a2887ad47b1048cc47d867ec6b8327f1ac
lucaspayne <lucaspayyne@gmail.com>
2024-05-31 17:17:18 +1200

    cleaning. removed symlinks from git, generate in setup_symlinks

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -5,34 +5,51 @@
 # todo:
 #    Ask for confirmation if a file would be deleted.
 
-ln -s -f "$(realpath vim/vimdir/)" ~/.vim
-ln -s -f "$(realpath vim/vimrc.vim)" ~/.vimrc
-ln -s -f "$(realpath emacs/infokey)" ~/.infokey
-ln -s -f "$(realpath shell/profile.sh)" ~/.profile
-ln -s -f "$(realpath shell/bashrc.sh)" ~/.bashrc
-ln -s -f "$(realpath shell/bash_aliases.sh)" ~/.bash_aliases
-ln -s -f "$(realpath shell/bash_profile.sh)" ~/.bash_profile
-ln -s -f "$(realpath shell/bash_logout.sh)" ~/.bash_logout
-ln -s -f "$(realpath gdb/gdbinit.gdb)" ~/.gdbinit
-ln -s -f "$(realpath gdb/)" ~/.gdb
-ln -s -f "$(realpath X/Xresources.txt)" ~/.Xresources
-ln -s -f "$(realpath X/xinitrc)" ~/.xinitrc
-ln -s -f "$(realpath X/xsession)" ~/.xsession
-ln -s -f "$(realpath screen/screenrc.txt)" ~/.screenrc
-ln -s -f "$(realpath screen/screendir)" ~/.screen
-ln -s -f "$(realpath readline/inputrc.txt)" ~/.inputrc
-ln -s -f "$(realpath pager/lesskey.txt)" ~/.lesskey
-ln -s -f "$(realpath git/gitconfig)" ~/.gitconfig
-ln -s -f "$(realpath git/tigrc)" ~/.tigrc
-ln -s -f "$(realpath i3)" ~/.i3
-ln -s -f "$(realpath scripts)" ~/scripts
-ln -s -f "$(realpath source-highlight)" ~/.source-highlight
-ln -s -f "$(realpath lf)" ~/.config/lf
-ln -s -f "$(realpath qutebrowser)" ~/.config/qutebrowser
-ln -s -f "$(realpath nvim)" ~/.config/nvim
-ln -s -f "$(realpath apt/dpkg.cfg)" ~/.dpkg.cfg
-ln -s -f "$(realpath freedesktop.org/autostart)" ~/.config/autostart
-ln -s -f "$(realpath cool-retro-term)" ~/.local/share/cool-retro-term
+# Setup symlinks on system to this config dir.
+ln -n -s -f "$(realpath vim/vimdir/)" ~/.vim
+ln -n -s -f "$(realpath vim/vimrc.vim)" ~/.vimrc
+ln -n -s -f "$(realpath emacs/infokey)" ~/.infokey
+ln -n -s -f "$(realpath shell/profile.sh)" ~/.profile
+ln -n -s -f "$(realpath shell/bashrc.sh)" ~/.bashrc
+ln -n -s -f "$(realpath shell/bash_aliases.sh)" ~/.bash_aliases
+ln -n -s -f "$(realpath shell/bash_profile.sh)" ~/.bash_profile
+ln -n -s -f "$(realpath shell/bash_logout.sh)" ~/.bash_logout
+ln -n -s -f "$(realpath gdb/gdbinit.gdb)" ~/.gdbinit
+ln -n -s -f "$(realpath gdb/)" ~/.gdb
+ln -n -s -f "$(realpath X/Xresources.txt)" ~/.Xresources
+ln -n -s -f "$(realpath X/xinitrc)" ~/.xinitrc
+ln -n -s -f "$(realpath X/xsession)" ~/.xsession
+ln -n -s -f "$(realpath screen/screenrc.txt)" ~/.screenrc
+ln -n -s -f "$(realpath screen/screendir)" ~/.screen
+ln -n -s -f "$(realpath readline/inputrc.txt)" ~/.inputrc
+ln -n -s -f "$(realpath pager/lesskey.txt)" ~/.lesskey
+ln -n -s -f "$(realpath git/gitconfig)" ~/.gitconfig
+ln -n -s -f "$(realpath git/tigrc)" ~/.tigrc
+ln -n -s -f "$(realpath i3)" ~/.i3
+ln -n -s -f "$(realpath scripts)" ~/scripts
+ln -n -s -f "$(realpath source-highlight)" ~/.source-highlight
+ln -n -s -f "$(realpath lf)" ~/.config/lf
+ln -n -s -f "$(realpath qutebrowser)" ~/.config/qutebrowser
+ln -n -s -f "$(realpath nvim)" ~/.config/nvim
+ln -n -s -f "$(realpath apt/dpkg.cfg)" ~/.dpkg.cfg
+ln -n -s -f "$(realpath freedesktop.org/autostart)" ~/.config/autostart
+ln -n -s -f "$(realpath cool-retro-term)" ~/.local/share/cool-retro-term
+
+# Setup symlinks in this directory to system files.
+# This is for convenient navigation of config. The directory contents, and the symlinks themselves, are not tracked by git.
+# The symlinks are not tracked because they could point to different places on different machines, e.g. with different usernames.
+ln -n -s -f /etc/sources.list apt/sources.list
+ln -n -s -f /etc/sources.list.d apt/sources.list.d
+ln -n -s -f /var/lib/apt apt/aptdir
+ln -n -s -f /var/lib/aptlogs apt/aptlogs
+ln -n -s -f /var/lib/dpkgdir apt/dpkgdir
+ln -n -s -f /usr/share/applications ./freedesktop.org/applications
+ln -n -s -f "$HOME/.local/share/applications" ./freedesktop.org/user_applications
+ln -n -s -f "$HOME/.local/lib/python3.10/" ./python/local_python3.10
+ln -n -s -f "$HOME/.python_history" ./python/python_history
+ln -n -s -f "$HOME/config/qutebrowser" ./qutebrowser/qutebrowser
+ln -n -s -f "$HOME/config/scripts" ./scripts/scripts
+ln -n -s -f "$HOME/.bash_history" ./shell/bash_history
 
 # ~/.local/share/applications.
 # The system saves files in here, so don't want to copy those to this repo.
