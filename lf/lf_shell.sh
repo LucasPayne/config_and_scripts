0499e215c905d797bd59713f057f8abf0f349efa
lucaspayne <lucaspayyne@gmail.com>
2024-11-24 01:37:17 +1300

    moved lfs to script

--- lf/lf_shell.sh
+++ lf/lf_shell.sh
@@ -30,9 +30,8 @@ lfcd ()
 
     local tmp="$(mktemp)"
     # `command` is needed in case `lfcd` is aliased to `lf`
-    command lf -command="load-selection \"$selection_file\"" -print-selection -selection-path="$selection_file" -last-dir-path="$tmp" "$@" &
-    local lf_server_id="$$"
-    fg
+    #command lf -command="set user_selection_file \"$selection_file\"; load-selection \"$selection_file\"" -last-dir-path="$tmp" "$@"
+    command lf -command="set user_selection_file \"$selection_file\";  \"$selection_file\"" -last-dir-path="$tmp" "$@"
 
     if [ -f "$tmp" ]; then
         dir="$(cat "$tmp")"
@@ -54,94 +53,3 @@ lfcd ()
 }
 
 # lf selection command
-lfs ()
-{
-    local lf_runtime_dir="$XDG_RUNTIME_DIR/lf"
-    local selection_file="$lf_runtime_dir/primary_selection"
-
-    if [ ! -f "$selection_file" ]
-    then
-        >&2 "error: no selection file"
-        return 1
-    fi
-
-    if [ ! -t 0 ]
-    then
-        # Input from a pipe.
-        # Interpret stdin as selections.
-        local append_mode
-        if [ $# -eq 1 ] && [ "$1" = -a ]
-        then
-            # append selections
-            append_mode=1
-        else
-            # overwrite selections
-            append_mode=0
-        fi
-
-        local selection
-        if [ $append_mode -eq 1 ]
-        then
-            selection="$(cat "$selection_file")"
-        else
-            selection=""
-        fi
-
-        # This block is only to help when handling lf's newline behaviour
-        # when writing the selection file.
-        local initial_empty=0
-        if [ -z "$selection" ]
-        then
-            initial_empty=1
-        fi
-
-        local out_index=0
-        while read -r line
-        do
-            if [ ! -e "$line" ]
-            then
-                >&2 echo "File \"$line\" doesn't exist, ignoring."
-            else
-                if [ $initial_empty -eq 0 ] && [ $out_index -eq 0 ]
-                then
-                    # Handle lf's newline behaviour when writing the selection file.
-                    selection="$selection"$'\n'
-                fi
-
-                local out_line="$(realpath "$line")"
-                selection="$selection"$'\n'"$out_line"
-                ((out_index++))
-            fi
-        done < <(cat)
-
-        # Write to selection file.
-        echo "$selection" > "$selection_file"
-        
-        # Remove last newline from selection file.
-        # (lf does this when it writes selection files itself.)
-        # This has no effect if the file is empty.
-        truncate -s -1 "$selection_file"
-        
-        exit 0
-    fi
-
-    if [ $# -eq 0 ]
-    then
-        if [ -s "$selection_file" ]
-        then
-            cat "$selection_file"
-            # lf is not adding a new-line
-            echo
-        fi
-    elif [ $# -eq 1 ]
-    then
-        if [ "$1" = "-c" ]
-        then
-            # Clear selection file
-            >"$selection_file"
-        fi
-    else
-        >&2 echo "bad args"
-        return 1
-    fi
-}
