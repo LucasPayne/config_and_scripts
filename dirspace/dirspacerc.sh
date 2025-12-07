b7ade063fd83890d0e537b3f7a3adcd193f1bfd0
lucaspayne <lucaspayyne@gmail.com>
2025-12-07 16:09:52 +1300

    working useful implementation of schematic with @ dirspace slot favorite

--- dirspace/dirspacerc.sh
+++ dirspace/dirspacerc.sh
@@ -1,9 +1,8 @@
 
 schematic="$CONFIG_DIR/dirspace/schematics/schematic.txt"
 
-$DIRSPACE_RUNTIME/slots
-
-cur_slot=0
+cur_slot=1
+init_slot=1
 cur_dir=
 cur_vim_servername=
 while IFS= read -r line
@@ -12,17 +11,31 @@ do
     then
         # Give the previous dirspace some time to initialize.
         # TODO: Speed up schematic loading.
-        if [ $cur_slot -ne 0 ]
+        # -- Is this needed for the vim commands to surely execute in the right
+        #     one before the next dirspace is opened?
+        #     Try uncomment if there are bugs.
+        # if [ $cur_slot -ne 1 ]
+        # then
+        #     sleep 1
+        # fi
+        
+        # Check for @ initial dirspace tag.
+        if [[ "$line" =~ ^@ ]]
         then
-            sleep 1
+            # Set as initial slot.
+            init_slot=$cur_slot
+            line="${line#@}"
         fi
 
         cur_dir="$line"
         cur_vim_servername="$(dirspace_open "$line")"
-        ((cur_slot++))
 
         # Give the dirspace some time to initialize.
         sleep 1
+    
+        # Set the slot
+        VIM_SERVERNAME="$cur_vim_servername" vim_remote_expr "SetDirspaceSlot($cur_slot)"
+        ((cur_slot++))
     else
         if [ -z "$cur_dir" ]
         then
@@ -36,6 +49,8 @@ do
         # VIM_SERVERNAME="$cur_vim_servername" vim_remote_expr "execute(\"tabnew \"..shellescape($(vim_escape_string "$file")))"
         VIM_SERVERNAME="$cur_vim_servername" vim_remote_expr "execute(\"tabnew $file\")"
         VIM_SERVERNAME="$cur_vim_servername" vim_remote_expr "execute(\"tabnext 1\")"
-        VIM_SERVERNAME="$cur_vim_servername" vim_remote_expr "SetDirspaceSlot($cur_slot)"
     fi
 done < <(cat "$schematic")
+
+# Switch to the init slot (default 1).
+screen -X select $init_slot
