23835d89f4751a85fa6079cffb59f10f18b9bd61
lucaspayne <lucaspayyne@gmail.com>
2025-12-07 15:52:33 +1300

    dirspace schematic basic, todo make faster, clean up, error handling, proper quoting

--- dirspace/dirspacerc.sh
+++ dirspace/dirspacerc.sh
@@ -1,16 +1,27 @@
 
 schematic="$CONFIG_DIR/dirspace/schematics/schematic.txt"
 
+$DIRSPACE_RUNTIME/slots
+
+cur_slot=0
 cur_dir=
 cur_vim_servername=
 while IFS= read -r line
 do
     if ! [[ "$line" =~ ^[[:space:]] ]]
     then
+        # Give the previous dirspace some time to initialize.
+        # TODO: Speed up schematic loading.
+        if [ $cur_slot -ne 0 ]
+        then
+            sleep 1
+        fi
+
         cur_dir="$line"
         cur_vim_servername="$(dirspace_open "$line")"
-        # Give it some time to initialize.
-        # TODO: Speed up schematic loading.
+        ((cur_slot++))
+
+        # Give the dirspace some time to initialize.
         sleep 1
     else
         if [ -z "$cur_dir" ]
@@ -20,7 +31,11 @@ do
         fi
 
         file="$(echo "$line" | sed 's/^[[:space:]]*//')"
-        VIM_SERVERNAME="$cur_vim_servername" vim_remote_expr "execute(\"tabnew \"..shellescape($(vim_escape_string "$file")))"
+        # TODO proper quoting
+        # --doesn't work
+        # VIM_SERVERNAME="$cur_vim_servername" vim_remote_expr "execute(\"tabnew \"..shellescape($(vim_escape_string "$file")))"
+        VIM_SERVERNAME="$cur_vim_servername" vim_remote_expr "execute(\"tabnew $file\")"
         VIM_SERVERNAME="$cur_vim_servername" vim_remote_expr "execute(\"tabnext 1\")"
+        VIM_SERVERNAME="$cur_vim_servername" vim_remote_expr "SetDirspaceSlot($cur_slot)"
     fi
 done < <(cat "$schematic")
